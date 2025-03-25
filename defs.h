0289080b7c718ebb0e3bfd767f14ad9ac236c38c
ngothithaovan <ngothithaovan.8@gmail.com>
2025-03-25 13:47:26 +0700

    aa

--- defs.h
+++ defs.h
@@ -1,11 +1,10 @@
 #ifndef DEFS__H
 #define  DEFS__H
 
- const int SCREEN_WIDTH=900;
- const int SCREEN_HEIGHT =700;
+ const int SCREEN_WIDTH=1000;
+ const int SCREEN_HEIGHT =900;
  #define WINDOW_TITLE "BAD ICE CREAM CUTE"
- const int TILE_SIZE = 50;  // Mỗi ô 50x50 pixels
- const int MAP_WIDTH = 14;  // 14 cột
- const int MAP_HEIGHT = 18; // 18 hàng
+const int MAP_WIDTH = 11;
+const int MAP_HEIGHT = 8;
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,66 +1,45 @@
 #include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <string>
 #include "defs.h"
 #include "graphics.h"
 
 using namespace std;
-//1 la tuong co dinh
-//0 la duong di
-//2 la tuong bang co the pha
-//3 la trai cay
-//4 la nhan vat
-int gameMap[MAP_HEIGHT][MAP_WIDTH] = {
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 1},
-    {1, 0, 3, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 1},
-    {1, 2, 0, 2, 0, 1, 1, 1, 0, 2, 0, 2, 0, 1},
-    {1, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 0, 1},
-    {1, 0, 1, 1, 0, 0, 4, 0, 0, 1, 1, 0, 0, 1},
-    {1, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 0, 1},
-    {1, 2, 0, 2, 0, 1, 1, 1, 0, 2, 0, 2, 0, 1},
-    {1, 0, 3, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 1},
-    {1, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 1},
-    {1, 0, 3, 0, 0, 0, 2, 0, 0, 0, 3, 0, 0, 1},
-    {1, 2, 0, 2, 0, 1, 1, 1, 0, 2, 0, 2, 0, 1},
-    {1, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 0, 1},
-    {1, 0, 1, 1, 0, 0, 4, 0, 0, 1, 1, 0, 0, 1},
-    {1, 0, 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 0, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+
+char levelData[MAP_HEIGHT][MAP_WIDTH] = {
+    "##########",
+    "#    F   #",
+    "#  ###   #",
+    "#    #   #",
+    "# P  # E #",
+    "#    #   #",
+    "#    F   #",
+    "##########"
 };
 
-void drawMap(SDL_Renderer* renderer, SDL_Texture* tileset) {
-    SDL_Rect srcRect, dstRect;
-    srcRect.w = srcRect.h = TILE_SIZE;
-    dstRect.w = dstRect.h = TILE_SIZE;
+
+void drawLevel(Graphics* graphics,SDL_Texture* ice ,SDL_Texture* fruit,SDL_Texture* enemy, SDL_Texture* player) {
+    int tileSize = 50;
 
     for (int y = 0; y < MAP_HEIGHT; y++) {
         for (int x = 0; x < MAP_WIDTH; x++) {
-            srcRect.x = gameMap[y][x] * TILE_SIZE;
-            srcRect.y = 0;
-            dstRect.x = x * TILE_SIZE;
-            dstRect.y = y * TILE_SIZE;
-            SDL_RenderCopy(renderer, tileset, &srcRect, &dstRect);
-        }
-    }
-}
+            SDL_Rect rect = {x * tileSize, y * tileSize, tileSize, tileSize};
 
-bool checkMouse(Graphics &graphics, SDL_Rect click) {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT) {
-            return false;
-        } else if (event.type == SDL_MOUSEBUTTONDOWN) {
-            int mouseX = event.button.x;
-            int mouseY = event.button.y;
-            if (graphics.checkClick(mouseX, mouseY, click)) {
-                return true;
+            SDL_Texture* texture = NULL;
+
+            switch (levelData[y][x]) {
+                case '#': texture = ice; break;
+                case 'F': texture = fruit; break;
+                case 'E': texture = enemy; break;
+                case 'P': texture = player; break;
+            }
+
+            if (texture != NULL) {
+                graphics->renderTexture( texture,rect.x,rect.y);
             }
         }
     }
-    return false;
 }
 
 int main(int argc, char* argv[]) {
@@ -71,29 +50,43 @@ int main(int argc, char* argv[]) {
     SDL_Texture *icecreamtitle = graphics.loadTexture("icecreamtitle.PNG");
     SDL_Texture *startgame = graphics.loadTexture("startgame.PNG");
     SDL_Texture *backgroundPlay = graphics.loadTexture("background_play.PNG");
+
+
     SDL_Texture *ice = graphics.loadTexture("ice.PNG");
-    bool running = true;
+    SDL_Texture *fruit = graphics.loadTexture("fruit.PNG");
+    SDL_Texture *enemy = graphics.loadTexture("enemy.PNG");
+    SDL_Texture *player = graphics.loadTexture("player.PNG");
+
+
+
+
+     bool running = true;
     int gameState = 0;
     SDL_Rect startButton = {380, 450, 200, 80};
-    while (running) {
 
+    while (running) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 running = false;
+            } else if (event.type == SDL_MOUSEBUTTONDOWN) {
+                int mouseX = event.button.x;
+                int mouseY = event.button.y;
+                if (mouseX >= startButton.x && mouseX <= startButton.x + startButton.w &&
+                    mouseY >= startButton.y && mouseY <= startButton.y + startButton.h) {
+                    gameState = 1;
+                }
             }
         }
+
         if (gameState == 0) {
             graphics.prepareScene(background);
             graphics.renderTexture(icecreamtitle, 150, 50);
             graphics.renderTexture(startgame, startButton.x, startButton.y);
             graphics.presentScene();
-            if(checkMouse(graphics,startButton)) {gameState=1;}
-        }
-        else if(gameState==1)
-        {
+        } else if (gameState == 1) {
             graphics.prepareScene(backgroundPlay);
-            graphics.renderTexture(ice, 0, 0);
+            drawLevel(&graphics, ice, fruit,enemy,player);
             graphics.presentScene();
         }
     }
