87e63a27367cb435d68e9c77c8b69b73b0b78c42
ngothithaovan <ngothithaovan.8@gmail.com>
2025-03-25 23:06:34 +0700

    aaa

--- defs.h
+++ defs.h
@@ -1,10 +1,12 @@
 #ifndef DEFS__H
 #define  DEFS__H
 
- const int SCREEN_WIDTH=1000;
- const int SCREEN_HEIGHT =900;
+ const int SCREEN_WIDTH=900;
+ const int SCREEN_HEIGHT =700;
  #define WINDOW_TITLE "BAD ICE CREAM CUTE"
-const int MAP_WIDTH = 11;
-const int MAP_HEIGHT = 8;
-
+const int MAP_WIDTH = 19;
+const int MAP_HEIGHT = 14;
+const int TILE_SIZE = 50;
+const int PLAYER_SPEED = 50;
+const Uint32 moveCooldown = 200;
 #endif
--- main.cpp
+++ main.cpp
@@ -5,34 +5,85 @@
 #include "defs.h"
 #include "graphics.h"
 
+
 using namespace std;
+/*
+# : Tường
 
+P : Người chơi
+
+E : Kẻ thù
+
+F : Trái cây
+*/
 char levelData[MAP_HEIGHT][MAP_WIDTH] = {
-    "##########",
-    "#    F   #",
-    "#  ###   #",
-    "#    #   #",
-    "# P  # E #",
-    "#    #   #",
-    "#    F   #",
-    "##########"
+    "##################",
+    "#P      #    F   #",
+    "#  #### #  ####  #",
+    "#  #       #  #  #",
+    "#  #  ######  #  #",
+    "#     #       #  #",
+    "##### #  ######  #",
+    "#     #    E     #",
+    "#  ###########   #",
+    "#  #  E  #   #   #",
+    "#  ####  #  ###  #",
+    "#     E     F    #",
+    "##################"
 };
+int playerRow = 1, playerCol = 1;
+int playerX = playerCol * TILE_SIZE, playerY = playerRow * TILE_SIZE;
+// Biến để kiểm soát tốc độ di chuyển
+Uint32 lastMoveTime = 0;  // Lưu thời gian lần di chuyển cuối
 
+// kiem tra co the di chuyen hay khong
+bool canMove(int newX, int newY) {
+    int newRow = newY / TILE_SIZE;
+    int newCol = newX / TILE_SIZE;
 
+    // Kiểm tra không vượt giới hạn bản đồ
+    if (newRow < 0 || newRow >= MAP_HEIGHT || newCol < 0 || newCol >= MAP_WIDTH) return false;
+
+    // Kiểm tra có tường cản đường không
+    return levelData[newRow][newCol] != '#';
+}
+// Xu ly hap phim di chuyen
+void handleInput(SDL_Event& event) {
+    Uint32 currentTime = SDL_GetTicks();  // Lấy thời gian hiện tại
+
+    if (event.type == SDL_KEYDOWN && (currentTime - lastMoveTime >= moveCooldown)) {
+       int newX = playerX, newY = playerY;
+
+        switch (event.key.keysym.sym) {
+            case SDLK_UP: newY --; break;
+            case SDLK_DOWN: newY++; break;
+            case SDLK_LEFT: newX--; break;
+            case SDLK_RIGHT: newX++; break;
+        }
+
+        if (canMove(newX, newY)) {
+            // Xóa nhân vật khỏi vị trí cũ
+            levelData[playerRow][playerCol] = ' ';
+            // Cập nhật vị trí mới
+            playerX = newX;
+            playerY = newY;
+            levelData[playerRow][playerCol] = 'P';
+
+            lastMoveTime = currentTime;  // Cập nhật thời gian di chuyển cuối
+        }
+    }
+}
 void drawLevel(Graphics* graphics,SDL_Texture* ice ,SDL_Texture* fruit,SDL_Texture* enemy, SDL_Texture* player) {
-    int tileSize = 50;
 
     for (int y = 0; y < MAP_HEIGHT; y++) {
         for (int x = 0; x < MAP_WIDTH; x++) {
-            SDL_Rect rect = {x * tileSize, y * tileSize, tileSize, tileSize};
-
+            SDL_Rect rect = {x * TILE_SIZE, y *TILE_SIZE, TILE_SIZE, TILE_SIZE};
             SDL_Texture* texture = NULL;
 
             switch (levelData[y][x]) {
                 case '#': texture = ice; break;
                 case 'F': texture = fruit; break;
                 case 'E': texture = enemy; break;
-                case 'P': texture = player; break;
             }
 
             if (texture != NULL) {
@@ -40,10 +91,12 @@ void drawLevel(Graphics* graphics,SDL_Texture* ice ,SDL_Texture* fruit,SDL_Textu
             }
         }
     }
+    graphics->renderTexture(player, playerX, playerY);
 }
 
 int main(int argc, char* argv[]) {
     Graphics graphics;
+    //Player player(&graphics);
     graphics.init();
 
     SDL_Texture *background = graphics.loadTexture("background_start.JPG");
@@ -51,11 +104,11 @@ int main(int argc, char* argv[]) {
     SDL_Texture *startgame = graphics.loadTexture("startgame.PNG");
     SDL_Texture *backgroundPlay = graphics.loadTexture("background_play.PNG");
 
-
-    SDL_Texture *ice = graphics.loadTexture("ice.PNG");
-    SDL_Texture *fruit = graphics.loadTexture("fruit.PNG");
-    SDL_Texture *enemy = graphics.loadTexture("enemy.PNG");
-    SDL_Texture *player = graphics.loadTexture("player.PNG");
+    //cac nhan vat trong game
+    SDL_Texture *icetexture = graphics.loadTexture("ice.PNG");
+    SDL_Texture *fruittexture = graphics.loadTexture("fruit.PNG");
+    SDL_Texture *enemytexture = graphics.loadTexture("enemy.PNG");
+    SDL_Texture *playertexture = graphics.loadTexture("player.PNG");
 
 
 
@@ -69,7 +122,8 @@ int main(int argc, char* argv[]) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 running = false;
-            } else if (event.type == SDL_MOUSEBUTTONDOWN) {
+            }
+            else if (event.type == SDL_MOUSEBUTTONDOWN) {
                 int mouseX = event.button.x;
                 int mouseY = event.button.y;
                 if (mouseX >= startButton.x && mouseX <= startButton.x + startButton.w &&
@@ -77,6 +131,7 @@ int main(int argc, char* argv[]) {
                     gameState = 1;
                 }
             }
+            handleInput(event);
         }
 
         if (gameState == 0) {
@@ -86,9 +141,10 @@ int main(int argc, char* argv[]) {
             graphics.presentScene();
         } else if (gameState == 1) {
             graphics.prepareScene(backgroundPlay);
-            drawLevel(&graphics, ice, fruit,enemy,player);
+            drawLevel(&graphics, icetexture, fruittexture,enemytexture,playertexture);
             graphics.presentScene();
         }
+        SDL_Delay(16);
     }
 
     SDL_DestroyTexture(background);
@@ -98,4 +154,5 @@ int main(int argc, char* argv[]) {
     graphics.quit();
     return 0;
 }
+
 
